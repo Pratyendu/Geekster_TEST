15f8dd6107fd4c307ecd814b60b615a68d3273f2
Pratyendu Sarkar <pratyendu@gmail.com>
2023-03-05 16:58:58 +0530

    Test 5th March

---
+++ March2023_test/nativeQueries/src/main/java/com/test/nativeQueries/NativeQueriesApplication.java
@@ -0,0 +1,13 @@
+package com.test.nativeQueries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeQueriesApplication.class, args);
+	}
+
+}
---
+++ March2023_test/nativeQueries/src/test/java/com/test/nativeQueries/NativeQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.nativeQueries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NativeQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
